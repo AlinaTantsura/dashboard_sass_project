154d00017cbd2e22119dc067e827dd230c28039a
Alina <alinatanst@gmail.com>
2024-09-06 13:26:07 +0300

    Add icons and utilities

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
-import "../scss/style.scss";
+import "../scss/main.scss";
+
+import * as bodyScrollLock from 'body-scroll-lock';
+
+//Sidebar menu
+const refsMenu = {
+    openMenuBtn: document.querySelector('.js-menu-open'),
+    closeMunuBtn: document.querySelector('js-menu-close'),
+    overlayMenu: document.querySelector('.js-menu'),
+};
+
+const toggleMenu = () => {
+    const isOpenMenu =
+        refsMenu.openMenuBtn.getAttribute('aria-expanded') === "true" || false;
+    refsMenu.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    refsMenu.overlayMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+};
+
+refsMenu.openMenuBtn.addEventListener('click', toggleMenu)
+refsMenu.closeMenuBtn.addEventListener('click', toggleMenu)
